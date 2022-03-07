dc12bba70d13385c21d4dffa7755d2d0d02cf849
kmj36 <kmj36953695@gmail.com>
2022-03-07 23:36:14 +0900

    [UPDATE] SaveDB init

--- wscmanager/src/apis/SaveDB.go
+++ wscmanager/src/apis/SaveDB.go
@@ -1,7 +1,36 @@
 package wsc_apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SaveDB(c *gin.Context) {
+	AuthorizaionHeader := c.GetHeader("Authorizaion")
 
+	if AuthorizaionHeader == "" || !strings.Contains(AuthorizaionHeader, "Bearer secret_") {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Unable to get Authorization header.",
+		})
+		return
+	}
+
+	var Crawljson SaveDBjson
+
+	binderr := c.BindJSON(&Crawljson)
+	if binderr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Unable to get body.",
+		})
+		return
+	}
+
+	if Crawljson.StartDate == "" || Crawljson.EndDate == "" || Crawljson.Target == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Insufficient parameters.",
+		})
+		return
+	}
 }

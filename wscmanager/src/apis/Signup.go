9ab96cbade94900b34428ed6eeed52ca58dec353
juseok <jusuck4523@gmail.com>
2022-03-08 00:08:20 -0800

    [UPDATE] add checkSignupKey func
    회원가입 시 key의 여부 및 key 값이 맞는지 확인

--- wscmanager/src/apis/Signup.go
+++ wscmanager/src/apis/Signup.go
@@ -1,7 +1,32 @@
 package wsc_apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	wsc_jsonstructs "wscmanager.com/jsonstructs"
+)
 
 func Signup(c *gin.Context) {
+	var input wsc_jsonstructs.Signupjson
+	//유효성 검사
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Missing id or password"})
+		return
+	}
+	// key 체크
+	err := godotenv.Load(".env")
 
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	secret := os.Getenv("KEY")
+	if key := input.Key; key != secret {
+		c.JSON(http.StatusForbidden, gin.H{"message": "Invalid key value."})
+		return
+	}
 }

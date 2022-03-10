5be82e36cb69b8a9178da15f0b2bf62fd02ebdee
juseok <jusuck4523@gmail.com>
2022-03-10 04:27:24 -0800

    [UPDATE] check input values
    id, pw 유효성 검사

--- wscmanager/src/apis/Login.go
+++ wscmanager/src/apis/Login.go
@@ -1,7 +1,18 @@
 package wsc_apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	wsc_jsonstructs "wscmanager.com/jsonstructs"
+)
 
 func Login(c *gin.Context) {
+	var input wsc_jsonstructs.Loginjson
 
+	//유효성 검사
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Missing id or password"})
+		return
+	}
 }

8a40686912dd33b75d96682886d4cc2d26c3d3cb
kmj36 <kmj36953695@gmail.com>
2022-03-12 15:12:02 +0900

    [UPDATE] page api parse

--- wscmanager/src/apis/SaveDB.go
+++ wscmanager/src/apis/SaveDB.go
@@ -87,14 +87,17 @@ func SaveDB(c *gin.Context) {
 		datedata, datecheck := data["results"].([]interface{})[i].(map[string]interface{})["properties"].(map[string]interface{})["FindDate"].(map[string]interface{})["date"].(map[string]interface{})
 		targetdata, targetcheck := data["results"].([]interface{})[i].(map[string]interface{})["properties"].(map[string]interface{})["Target"].(map[string]interface{})["select"].(map[string]interface{})
 		humandata, humancheck := data["results"].([]interface{})[i].(map[string]interface{})["properties"].(map[string]interface{})["Human"].(map[string]interface{})["people"].([]interface{})
+		pageid, pageidcheck := data["results"].([]interface{})[i].(map[string]interface{})["id"]
 		var sqltitledata string = ""
 		var sqltypedata string = ""
 		var sqlvulnerabilitydata string = ""
 		var sqldatedata string = ""
 		var sqltargetdata string = ""
 		var sqlhumandata string = ""
+		var sqldescription string = ""
+		var sqlreference string = ""
 
-		if !(titlecheck && typecheck && vulnerabilitycheck && datecheck && targetcheck && humancheck) {
+		if !(titlecheck && typecheck && vulnerabilitycheck && datecheck && targetcheck && humancheck && pageidcheck) {
 			continue
 		}
 
@@ -127,7 +130,43 @@ func SaveDB(c *gin.Context) {
 		sqltypedata = typedata["name"].(string)
 		sqlvulnerabilitydata = vulnerabilitydata[0].(map[string]interface{})["name"].(string)
 
+		pageapiurl := "https://api.notion.com/v1/blocks/" + pageid.(string) + "/children"
+
+		pageapireq, pageapireqdoerr := http.NewRequest("GET", pageapiurl, nil)
+		if pageapireqdoerr != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+
+		pageapireq.Header.Add("Accept", "application/json")
+		pageapireq.Header.Add("Notion-Version", "2022-02-22")
+		pageapireq.Header.Add("Authorization", AuthorizaionHeader)
+
+		pageapires, pageapiresdoerr := http.DefaultClient.Do(pageapireq)
+		if pageapiresdoerr != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		defer pageapires.Body.Close()
+
+		pageapibody, pageapireaderr := ioutil.ReadAll(pageapires.Body)
+		if pageapireaderr != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		pagedata := make(map[string]interface{})
+
+		pageapimarshalerr := json.Unmarshal(pageapibody, &pagedata)
+		if pageapimarshalerr != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		// [TODO] : 문자열 이을때 개행문자 추가
+
 		// [TODO] : sql 서버 쿼리 작업
+
 	}
 
 	c.Status(http.StatusOK)

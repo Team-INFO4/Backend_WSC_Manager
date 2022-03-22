028226fa2967239c4ecc4831da02672a5d4073b8
juseok <jusuck4523@gmail.com>
2022-03-21 22:42:33 -0700

    [UPDATE] add token verification function
    토큰 검증 함수 추가

--- wscmanager/src/utils/token/token.go
+++ wscmanager/src/utils/token/token.go
@@ -1,11 +1,14 @@
 package token
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 func CreateJWT(user_id string) (string, error) {
@@ -21,4 +24,41 @@ func CreateJWT(user_id string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ExtractToken(c *gin.Context) string {
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func VerifyToken(c *gin.Context) (*jwt.Token, error) {
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+func TokenValid(c *gin.Context) (string, error) {
+	token, err := VerifyToken(c)
+	if err != nil {
+		return "", err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		id, ok := claims["user_id"].(string)
+		if !ok {
+			return "", err
+		}
+		return id, nil
+	}
+	return "", err
 }
